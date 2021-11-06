0db1d53789ea149b0c18bdd49341c10a7ac5b111
Pavithra Balasubramaniam <pavis.balu@gmail.com>
2021-11-07 00:44:28 +0530

    Adding the document meta

---
+++ src/main/java/edu/bits/wilp/ir_assignment/index/DocumentMeta.java
@@ -0,0 +1,22 @@
+package edu.bits.wilp.ir_assignment.index;
+
+import java.util.Map;
+import java.util.Set;
+
+public class DocumentMeta {
+    private final Map<String, Double> tf;
+    public final int docId;
+
+    public DocumentMeta(Map<String, Double> tf, int docId) {
+        this.tf = tf;
+        this.docId = docId;
+    }
+
+    public Set<String> tokens() {
+        return tf.keySet();
+    }
+
+    public Double tf(String token) {
+        return tf.getOrDefault(token, 0.0);
+    }
+}
