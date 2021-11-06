76ac0e75e4de3d4e50ac93ea339d6a0d16d585d5
Pavithra Balasubramaniam <pavis.balu@gmail.com>
2021-11-06 22:16:54 +0530

    WIP - TF-IDF implementation

--- src/main/java/edu/bits/wilp/ir_assignment/indexing/Indexer.java
+++ src/main/java/edu/bits/wilp/ir_assignment/indexing/Indexer.java
@@ -7,10 +7,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Indexer {
     public static void main(String[] args) throws IOException {
@@ -22,45 +19,52 @@ public class Indexer {
         Reader in = new FileReader(filename);
         Iterable<CSVRecord> records = csvFormat().parse(in);
         int docId = 0;
-        Map<String, Double> TF = new HashMap<>();
-        Map<String, List<Integer>> documentToTerms = new HashMap<>();
+        List<DocumentMeta> documents = new ArrayList<>();
         for (CSVRecord record : records) {
             String reviewsText = record.get("reviews.text");
             DocumentTokenizer document = new DocumentTokenizer(List.of(reviewsText));
             document.process();
             Map<String, Double> docTf = document.tf();
-
-            TF = DocumentTokenizer.mergeCounters().apply(TF, docTf);
-            for (String token : document.tokens()) {
-                List<Integer> existingDocuments = documentToTerms.getOrDefault(token, new ArrayList<>());
-                existingDocuments.add(docId);
-                documentToTerms.put(token, existingDocuments);
-            }
+            documents.add(new DocumentMeta(docTf, docId));
 
             // update the docId, so we can consider each review as a new document
             docId++;
         }
 
-        int totalDocuments = docId;
-        Map<String, Double> DF = computeDF(documentToTerms, totalDocuments);
-        Map<String, Double> IDF = computeIDF(totalDocuments, DF);
-    }
+        final int N = documents.size();
+        Map<String, Double> DF = computeDF(documents);
+        Set<String> VOCAB = DF.keySet();
 
-    private Map<String, Double> computeIDF(int totalDocuments, Map<String, Double> DF) {
-        Map<String, Double> IDF = new HashMap<>();
-        for (String term : DF.keySet()) {
-            double idf = (double) totalDocuments / DF.get(term);
-            IDF.put(term, idf);
+        for (DocumentMeta document : documents) {
+            Set<String> tokens = document.tokens();
+            for (String token : tokens) {
+                double tf = document.tf(token);
+                double df = DF.get(token);
+                double idf = Math.log(N + 1 / df + 1);
+                double tfIdf = tf * idf;
+                // TODO: Store these TF-IDF values separately and persist them in a file for later consumption
+                System.out.println(token + "@" + document.docId + " - tf-idf:" + tfIdf);
+            }
         }
-        return IDF;
     }
 
-    private Map<String, Double> computeDF(Map<String, List<Integer>> documentToTerms, int totalDocuments) {
+    private Map<String, Double> computeDF(List<DocumentMeta> documents) {
         Map<String, Double> DF = new HashMap<>();
+        Map<String, List<Integer>> termToDocuments = new HashMap<>();
         // df(t) = occurrence of t in N documents
         // to normalize the value, we also divide the occurrence by total number of documents
-        for (String token : documentToTerms.keySet()) {
-            DF.put(token, (double) documentToTerms.get(token).size() / totalDocuments);
+        for (DocumentMeta doc : documents) {
+            Set<String> tokens = doc.tokens();
+            for (String token : tokens) {
+                List<Integer> existingDocuments = termToDocuments.getOrDefault(token, new ArrayList<>());
+                existingDocuments.add(doc.docId);
+                termToDocuments.put(token, existingDocuments);
+            }
+        }
+
+        final int N = documents.size();
+        for (String term : termToDocuments.keySet()) {
+            DF.put(term, (double) termToDocuments.get(term).size() / N);
         }
         return DF;
     }
