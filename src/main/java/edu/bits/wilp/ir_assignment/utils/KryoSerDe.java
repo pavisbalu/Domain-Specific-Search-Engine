fe9fa257681bb424fcd8861a2cf1de656228efe2
Pavithra Balasubramaniam <pavis.balu@gmail.com>
2021-11-07 01:29:58 +0530

    KryoSerDe to persist TfIdf objects as the model

---
+++ src/main/java/edu/bits/wilp/ir_assignment/utils/KryoSerDe.java
@@ -0,0 +1,20 @@
+package edu.bits.wilp.ir_assignment.utils;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.io.OutputStream;
+
+public class KryoSerDe {
+    private final static Kryo kryo = new Kryo();
+
+    static {
+        kryo.setRegistrationRequired(false);
+    }
+
+    public static void writeToFile(Object value, OutputStream outputStream) {
+        Output output = new Output(outputStream);
+        kryo.writeObject(output, value);
+        output.close();
+    }
+}
