0be1602875ec29b5f61569a870ebef773a26d34f
Pavithra Balasubramaniam <pavis.balu@gmail.com>
2021-11-07 00:35:39 +0530

    Adding cosine similarity impl

--- src/main/java/edu/bits/wilp/ir_assignment/utils/NumberUtil.java
+++ src/main/java/edu/bits/wilp/ir_assignment/utils/NumberUtil.java
@@ -9,4 +9,22 @@ public class NumberUtil {
         df.setRoundingMode(RoundingMode.CEILING);
         return Double.parseDouble(df.format(value));
     }
+
+    // Ref - http://www.java2s.com/example/java-utility-method/cosine-similarity/cosinesim-double-a-double-b-e9dbc.html
+    public static double cosineSim(double[] a, double[] b) {
+        if (a == null || b == null || a.length < 1 || b.length < 1 || a.length != b.length)
+            return Double.NaN;
+
+        double sum = 0.0, sum_a = 0, sum_b = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i] * b[i];
+            sum_a += a[i] * a[i];
+            sum_b += b[i] * b[i];
+        }
+
+        double val = Math.sqrt(sum_a) * Math.sqrt(sum_b);
+
+        return sum / val;
+    }
+
 }
