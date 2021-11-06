efc814026a6fdc7477f2eb8475fbf7369df0a4ce
Pavithra Balasubramaniam <pavis.balu@gmail.com>
2021-11-06 19:29:23 +0530

    Adding Stemmer and StemmerTest

---
+++ src/main/java/edu/bits/wilp/ir_assignment/tokenizing/Stemmer.java
@@ -0,0 +1,11 @@
+package edu.bits.wilp.ir_assignment.tokenizing;
+
+import ca.rmen.porterstemmer.PorterStemmer;
+
+public class Stemmer {
+    private static final PorterStemmer stemmer = new PorterStemmer();
+
+    public static String stemWord(String input) {
+        return stemmer.stemWord(input);
+    }
+}
---
+++ src/test/java/edu/bits/wilp/ir_assignment/tokenizing/StemmerTest.java
@@ -0,0 +1,22 @@
+package edu.bits.wilp.ir_assignment.tokenizing;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class StemmerTest {
+    @Test
+    public void testStemWord() {
+        // testing nouns -- should remain unchanged
+        assertThat(Stemmer.stemWord("dog"), is("dog"));
+        assertThat(Stemmer.stemWord("queen"), is("queen"));
+        assertThat(Stemmer.stemWord("king"), is("king"));
+        // testing variations of adverb, adjectives, etc. which should get stemmed
+        assertThat(Stemmer.stemWord("likely"), is("like"));
+        assertThat(Stemmer.stemWord("liked"), is("like"));
+        assertThat(Stemmer.stemWord("liking"), is("like"));
+        assertThat(Stemmer.stemWord("likes"), is("like"));
+    }
+
+}
