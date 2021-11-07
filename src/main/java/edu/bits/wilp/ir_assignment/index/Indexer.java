65578d9a8929d7b67b7bdaf804fe7de568cdbd4c
Pavithra Balasubramaniam <pavis.balu@gmail.com>
2021-11-07 09:14:48 +0530

    Adding shade plugin for uber jar + persisting documents as well

--- src/main/java/edu/bits/wilp/ir_assignment/index/Indexer.java
+++ src/main/java/edu/bits/wilp/ir_assignment/index/Indexer.java
@@ -5,23 +5,30 @@ import edu.bits.wilp.ir_assignment.utils.KryoSerDe;
 import edu.bits.wilp.ir_assignment.utils.NumberUtil;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.*;
 
 public class Indexer {
+    private static final Logger LOG = LoggerFactory.getLogger(Indexer.class);
+
     public static void main(String[] args) throws IOException {
-        new Indexer().index("datasets/sample.csv", "reviews.text", "output.bin");
+        new Indexer().index("datasets/sample.csv", "reviews.text", "output.bin", "documents.bin");
     }
 
-    public void index(String filename, String inputFile, String outputFile) throws IOException {
+    public void index(String filename, String inputFile, String modelFile, String documentsFile) throws IOException {
+        LOG.info("Opening " + filename + " for reading");
         Reader in = new FileReader(filename);
         Iterable<CSVRecord> records = csvFormat().parse(in);
-
+        LOG.info("Files read, starting to compute DF");
         List<DocumentMeta> documents = processDocuments(inputFile, records);
 
         final int N = documents.size();
         Map<String, Double> DF = computeDF(documents);
+        LOG.info("DF Complete");
+        LOG.info("Starting Tf-Idf calculations");
         TfIdf tfIdfTable = new TfIdf(N, DF);
 
         // compute tf-idf
@@ -36,8 +43,10 @@ public class Indexer {
                 tfIdfTable.add(document.docId, token, tfIdf);
             }
         }
+        LOG.info("Tf-Idf calculations, done. Writing the model file: " + modelFile);
 
-        KryoSerDe.writeToFile(tfIdfTable, outputFile);
+        KryoSerDe.writeToFile(tfIdfTable, modelFile);
+        KryoSerDe.writeToFile(documents, documentsFile);
     }
 
     private List<DocumentMeta> processDocuments(String fieldName, Iterable<CSVRecord> records) {
