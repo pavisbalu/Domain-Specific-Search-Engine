9b4f4b713604aec5354507bba9398c7d17f4e7ca
Pavithra Balasubramaniam <pavis.balu@gmail.com>
2021-11-08 08:08:21 +0530

    Implemented a Document model for indexing

---
+++ src/main/java/edu/bits/wilp/ir_assignment/model/Field.java
@@ -0,0 +1,26 @@
+package edu.bits.wilp.ir_assignment.model;
+
+public class Field {
+    private final String key;
+    private final String value;
+    private final boolean isIndexed;
+
+    // for kryo
+    private Field(boolean isIndexed) {
+        this("", "", false);
+    }
+
+    public Field(String key, String value, boolean isIndexed) {
+        this.key = key;
+        this.value = value;
+        this.isIndexed = isIndexed;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
